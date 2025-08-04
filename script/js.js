9f5955798ec8e72870776c1e46312add0b00bfac
woddlrk <woddlrk@naver.com>
2025-08-04 17:07:52 +0900

    fix: GNB 드롭다운 떨림 완전 방지 및 화면 전체 너비 동적 적용

--- script/js.js
+++ script/js.js
@@ -1,3 +1,12 @@
-document.querySelector('button').addEventListener('click',function(){
-    document.querySelector('.popup').style.display='none';
+document.querySelector('button').addEventListener('click', function () {
+    document.querySelector('.popup').style.display = 'none';
+})
+
+$('.main-nav ul li:has(.gnb-dropdown)').on({
+    'mouseenter': function () {
+        $('header').addClass('on');
+
+    }, 'mouseleave': function () {
+        $('header').removeClass('on');
+    }
 })
